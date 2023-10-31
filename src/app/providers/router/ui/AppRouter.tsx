c7f3b402eb1e7639f0a12e64016ddee819e48464
Paavveel <pa.tomilin@gmail.com>
2023-10-31 22:53:41 +0300

    feat: add AppRouter

---
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -0,0 +1,18 @@
+import React, { FC } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react';
+import { RouteConfig } from 'shared/config/routeConfig/routeConfig';
+
+interface AppRouterProps {}
+
+export const AppRouter: FC<AppRouterProps> = ({}) => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        {Object.values(RouteConfig).map(({ path, element }) => (
+          <Route key={path} path={path} element={element} />
+        ))}
+      </Routes>
+    </Suspense>
+  );
+};
