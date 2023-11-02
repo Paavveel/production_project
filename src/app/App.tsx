35126f9ac9f3c27652997dbb5d88804c3f7b91ae
Paavveel <pa.tomilin@gmail.com>
2023-11-02 16:54:46 +0300

    feat: add i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,17 +4,37 @@ import { AppRouter } from './providers/router';
 import { NavBar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { useTheme } from './providers/ThemeProvider/lib/useTheme';
+import { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
+
+const Component = () => {
+  const { t, i18n } = useTranslation();
+
+  const toggle = () => {
+    i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
+  };
+
+  return (
+    <div>
+      <button onClick={toggle}>{t('Перевод')}</button>
+      {t('Тестовый пример')}
+    </div>
+  );
+};
 
 export const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <NavBar />
-      <div className='content-page'>
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback=''>
+        <NavBar />
+        <Component />
+        <div className='content-page'>
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
