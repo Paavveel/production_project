80253d9ed0991dac6f8a310a626ea34d0f8a0b5d
Paavveel <pa.tomilin@gmail.com>
2023-11-02 22:05:54 +0300

    chore: add stylelint and fix files

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -4,7 +4,6 @@ import LightIcon from 'shared/assets/icons/theme-light.svg';
 import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import cls from './ThemeSwitcher.module.scss';
 
 interface ThemeSwitcherProps {
   className?: string;
@@ -13,11 +12,7 @@ interface ThemeSwitcherProps {
 export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ className }) => {
   const { theme, toggleTheme } = useTheme();
   return (
-    <Button
-      className={classNames(cls.ThemeSwitcher, {}, [className])}
-      theme={ThemeButton.CLEAR}
-      onClick={toggleTheme}
-    >
+    <Button className={classNames('', {}, [className])} theme={ThemeButton.CLEAR} onClick={toggleTheme}>
       {theme === Theme.DARK ? <DarkIcon /> : <LightIcon />}
     </Button>
   );
