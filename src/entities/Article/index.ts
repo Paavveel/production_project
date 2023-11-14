07c7991ad32a9a5a10812c4bf31193ee55eed76c
Paavveel <pa.tomilin@gmail.com>
2023-11-14 22:24:59 +0300

    feat: add ArticleViewSelector

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -4,6 +4,7 @@ export type { Article } from './model/types/article';
 export type { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
 export { ArticleDetails } from './ui/ArticleDetails/ArticleDetails';
 export { ArticleList } from './ui/ArticleList/ArticleList';
+export { ArticleViewSelector } from './ui/ArticleViewSelector/ArticleViewSelector';
 
 export {
   getArticleDetailsData,
---
+++ src/entities/Article/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -0,0 +1,35 @@
+import { memo } from 'react';
+import ListIcon from 'shared/assets/icons/list-24-24.svg';
+import TiledIcon from 'shared/assets/icons/tiled-24-24.svg';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Icon } from 'shared/ui/Icon/Icon';
+import { ArticleView } from '../../model/types/article';
+import cls from './ArticleViewSelector.module.scss';
+
+interface ArticleViewSelectorProps {
+  className?: string;
+  view: ArticleView;
+  onViewClick?: (view: ArticleView) => void;
+}
+
+const viewTypes = [
+  { view: ArticleView.SMALL, icon: TiledIcon },
+  { view: ArticleView.BIG, icon: ListIcon },
+];
+
+export const ArticleViewSelector = memo(({ className, view, onViewClick }: ArticleViewSelectorProps) => {
+  const onClick = (newView: ArticleView) => () => {
+    onViewClick?.(newView);
+  };
+
+  return (
+    <div className={classNames(cls.ArticleViewSelector, {}, [className])}>
+      {viewTypes.map((viewType, i) => (
+        <Button key={i} theme={ButtonTheme.CLEAR} onClick={onClick(viewType.view)}>
+          <Icon className={classNames('', { [cls.selected]: viewType.view === view })} Svg={viewType.icon} />
+        </Button>
+      ))}
+    </div>
+  );
+});
