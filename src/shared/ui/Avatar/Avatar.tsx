b67d9de91b33302a4a34bb34dd6f687249dcc29e
Paavveel <pa.tomilin@gmail.com>
2023-12-11 20:10:14 +0300

    chore: add eslint unused imports plugin

--- src/shared/ui/Avatar/Avatar.tsx
+++ src/shared/ui/Avatar/Avatar.tsx
@@ -1,6 +1,5 @@
 import { CSSProperties, FC, useMemo } from 'react';
-import { useTranslation } from 'react-i18next';
-import { Mods, classNames } from '@/shared/lib/classNames/classNames';
+import { classNames, Mods } from '@/shared/lib/classNames/classNames';
 import cls from './Avatar.module.scss';
 
 interface AvatarProps {
