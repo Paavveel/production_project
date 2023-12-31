fb6723fa3cede24a6332a69de5ba33806e8437d2
Paavveel <pa.tomilin@gmail.com>
2023-12-31 14:32:45 +0300

    feat: add tests for isolated components with cypress

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -5,21 +5,39 @@ import { MemoryRouter } from 'react-router-dom';
 import { ReducersMapObject } from '@reduxjs/toolkit';
 import i18nForTests from '@/shared/config/i18n/i18nForTests';
 import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
+import { Theme } from '@/shared/const/theme';
+import { ThemeProvider } from '@/app/providers/ThemeProvider';
+import '@/app/styles/index.scss';
 
-export interface componentRenderOptions {
+export interface ComponentRenderOptions {
   route?: string;
   initialState?: DeepPartial<StateSchema>;
   asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
 }
 
-export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
+interface TestProviderProps {
+  children: ReactNode;
+  options?: ComponentRenderOptions;
+  theme?: Theme;
+}
+
+export const TestProvider = (props: TestProviderProps) => {
+  const { children, options = {}, theme = Theme.LIGHT } = props;
   const { route = '/', initialState, asyncReducers } = options;
 
-  return render(
+  return (
     <MemoryRouter initialEntries={[route]}>
       <StoreProvider initialState={initialState} asyncReducers={asyncReducers}>
-        <I18nextProvider i18n={i18nForTests}>{component}</I18nextProvider>
+        <ThemeProvider initialTheme={theme}>
+          <div className={`app ${theme}`}>
+            <I18nextProvider i18n={i18nForTests}>{children}</I18nextProvider>
+          </div>
+        </ThemeProvider>
       </StoreProvider>
     </MemoryRouter>
   );
+};
+
+export function componentRender(component: ReactNode, options: ComponentRenderOptions = {}) {
+  return render(<TestProvider options={options}>{component}</TestProvider>);
 }
